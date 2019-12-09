2872d182e8f0bd739642cb7166d28b6f1ff30c5f
Alice <alice.sabot@edu.ece.fr>
2019-12-09 17:30:46 +0100

    page admin css OK

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Formulaire from './Formulaire';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-
+import { Container, Row, Col } from 'reactstrap';
+import './Admin.css';
 
 
 class Admin extends React.Component{ 
@@ -11,8 +12,10 @@ class Admin extends React.Component{
       <div>
          
         <h3>Bienvenue dans l'espace admin !</h3>
-        <Link to="/Utilisateurs" className="link1">Liste Utilisateurs</Link>
-        <Link to="/Formulaire" className="link1">Nouvel Utilisateurs</Link>
+        <Row>
+          <Col sm={{ size: 3, offset: 4 }}><Link to="/Utilisateurs" className="utilisateur">Liste Utilisateurs</Link></Col>
+          <Col sm={3}><Link to="/Formulaire" className="formulaire">Nouvel Utilisateurs</Link></Col>
+        </Row>
       </div>
       );
     }
