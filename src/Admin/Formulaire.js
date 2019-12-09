5eed97611c9ec96ce76e870f9f8e175ccdb07aa7
Alice <alice.sabot@edu.ece.fr>
2019-12-09 16:02:58 +0100

    formulaire ok

--- src/Admin/Formulaire.js
+++ src/Admin/Formulaire.js
@@ -3,29 +3,47 @@ import React from 'react';
 class Formulaire extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {localisation: '', nb: '', taille: ''};
   
       this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+    
     }
   
     handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+      this.setState({localisation: event.target.localisation});
+      this.setState({nb: event.target.nb});
+      this.setState({taille: event.target.taille});
+
     }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
+        <form>
+
+          <h3>Nouveau Utilisateur</h3>
+
           <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <h5>Localisation: </h5>
+            <input type="text" localisation={this.state.localisation} onChange={this.handleChange} />
           </label>
-          <input type="submit" value="Submit" />
+
+          <label>
+            <h5>Nombre de personnes dans la maison: </h5>
+            <input type="number" nb={this.state.nb} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            <h5>Taille de la maison: </h5>
+            <select taille={this.state.taille} onChange={this.handleChange}>
+            <option taille="small">Small</option>
+            <option taille="medium">Medium</option>
+            <option taille="big">Big</option>
+          </select>
+
+          </label>
+          
+          <input type="submit" localisation ="submit" nb="submit" taille="Submit" />
+          
         </form>
       );
     }
