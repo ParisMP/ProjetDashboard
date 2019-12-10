7814f49506555c83a08c05ae08b00a74f7ef4a61
Alice <alice.sabot@edu.ece.fr>
2019-12-10 14:22:22 +0100

    header css OK

--- src/Widgets/TopNavBar/TopNavBar.js
+++ src/Widgets/TopNavBar/TopNavBar.js
@@ -26,7 +26,7 @@ const Topnav = props => {
     return (
         <div>
           <Navbar className="header" light expand="md">
-            <Col sm={1} md={3} lg={2} xl={2}><NavbarBrand href="/">MY DASHBOARD</NavbarBrand></Col>
+            <Col sm={2} md={3} lg={2} xl={2}><NavbarBrand href="/">MY DASHBOARD</NavbarBrand></Col>
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
               <Nav className="mr-auto" navbar>
@@ -42,10 +42,10 @@ const Topnav = props => {
                   </DropdownToggle>
                   <DropdownMenu left>
                     <DropdownItem>
-                        <Link to="/Utilisateurs" className="menuL">Liste des utilisateurs</Link>
+                    <NavLink href="/Utilisateurs/">Liste des utilisateurs</NavLink>
                     </DropdownItem>
                     <DropdownItem>
-                        <Link to="/Formulaire" className="menuF">Ajouter un utilisateur</Link>
+                    <NavLink href="/Formulaire/">Ajouter un utilisateur</NavLink>
                     </DropdownItem>
                   </DropdownMenu>
                 </UncontrolledDropdown>
