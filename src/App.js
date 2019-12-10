df47a3d3e8fc57c89dc25413d6b72457b0054eaf
Alice <alice.sabot@edu.ece.fr>
2019-12-10 13:46:35 +0100

    test header reactstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Topnav  from './Widgets/TopNavBar/TopNavBar';
 import Nav from './Widgets/Nav/Nav';
-
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import Admin from './Admin/Admin'
 import Widgets from './Widgets/Widgets'
@@ -14,20 +13,21 @@ class App extends Component {
   render()
   {
 
-    // {} will allow to reference the variables if dropdown button is clicked
-    // don't put () next to clickDropDown or the method would be automatically executed
-    //instead we pass a reference, we pass the address to that method
+
     return (
 
       <BrowserRouter>
+
+        <Topnav/>
+        <Nav/>
 
         <Route path="/Admin" component={Admin}/>
         <Route path="/Widgets" component={Widgets}/>
         <Route path="/Utilisateurs" component={Utilisateurs}/>
         <Route path="/Formulaire" component={Formulaire}/>
 
-          <Topnav/>
-          <Nav/>
+          
+  
   </BrowserRouter>
     );
   }
--- src/Widgets/TopNavBar/TopNavBar.js
+++ src/Widgets/TopNavBar/TopNavBar.js
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TopNavBar.css';
 import { Container, Row, Col } from 'reactstrap';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+  } from 'reactstrap';
 
+const Topnav = props => {
 
+    const [isOpen, setIsOpen] = useState(false);
 
-const Topnav = props => (
+    const toggle = () => setIsOpen(!isOpen);
 
-    <Container className="Topbar">
-        
-         <Row>
-            <Col><b href="/">MUSIC DASHBOARD</b></Col>
-            <Link to="/Admin" className="link1">Admin</Link>
-            <Link to="/Widgets" className="link1">Widgets</Link>
-         </Row>
+    return (
+        <div>
+          <Navbar className="header" light expand="md">
+            <Col sm={4} md={4} lg={4} xl={4}><NavbarBrand href="/">MY DASHBOARD</NavbarBrand></Col>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem className="menuW">
+                <Link to="/Widgets">Widgets</Link>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Admin
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    <DropdownItem>
+                        <Link to="/Utilisateurs" className="menuL">Liste des utilisateurs</Link>
+                    </DropdownItem>
+                    <DropdownItem>
+                        <Link to="/Formulaire" className="menuF">Ajouter un utilisateur</Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+
     
-       
-    </Container>
-);
+};
 
 export default Topnav;
