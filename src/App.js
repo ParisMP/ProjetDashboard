5bacb5f64c50227d57c9df7a1542d8206aaa6db1
Alice <alice.sabot@edu.ece.fr>
2019-12-10 17:23:53 +0100

    version +

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 import Topnav  from './Widgets/TopNavBar/TopNavBar';
 import Nav from './Widgets/Nav/Nav';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-import Admin from './Admin/Admin'
 import Widgets from './Widgets/Widgets'
 import Utilisateurs from './Admin/Utilisateurs'
-import Formulaire from './Admin/Formulaire'
+import Formulaire from './Admin/Formulaire/Formulaire'
 
 class App extends Component {
 
@@ -20,8 +19,6 @@ class App extends Component {
 
         <Topnav/>
         
-
-        <Route path="/Admin" component={Admin}/>
         <Route path="/Widgets" component={Widgets}/>
         <Route path="/Utilisateurs" component={Utilisateurs}/>
         <Route path="/Formulaire" component={Formulaire}/>
--- src/Widgets/Widgets.js
+++ src/Widgets/Widgets.js
@@ -7,22 +7,22 @@ import ShapeBChartW from './ShapeBarChartW/ShapeBarChartW';
 import Pro from './Profiles/Profiles';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
+import './Widgets.css';
 
 class Widgets extends React.Component{ 
   
     render(){
       return( 
-      <div>
+      <div >
             
-            <Row>
-              <Col sm={12} md={6} lg={4} xl={4}><RadialBChart/></Col>
-              <Col sm={12} md={6} lg={4} xl={4}><Example/></Col>
-              <Col sm={12} md={6} lg={4} xl={4}><Widget2/></Col>
-            </Row>
-            <Row>
-              <Col sm={12} md={6} lg={4} xl={4}><ShapeBChart/></Col>
-              <Col sm={12} md={6} lg={4} xl={4}><ShapeBChartW/></Col>
-              <Col sm={12} md={6} lg={4} xl={4}><Pro/></Col>
+            <Row >
+              <Col className="widget1" sm={12} md={6} lg={4} xl={4}><RadialBChart/></Col>
+              <Col className="widget1" sm={12} md={6} lg={4} xl={4}><Example/></Col>
+              <Col className="widget1" sm={12} md={6} lg={4} xl={4}><Widget2/></Col>
+
+              <Col className="widget1" sm={12} md={6} lg={4} xl={4}><ShapeBChart/></Col>
+              <Col className="widget1" sm={12} md={6} lg={4} xl={4}><ShapeBChartW/></Col>
+              <Col className="widget1" sm={12} md={6} lg={4} xl={4}><Pro/></Col>
             </Row>
           
       </div>
